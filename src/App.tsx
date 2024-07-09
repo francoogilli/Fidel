cc74f71ec76c9ce390397111ea8699a0efc41d6f
Franco Gilli <francogilli10@gmail.com>
2024-07-09 14:48:52 -0300

    Agregar background linear

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import Hero from "./components/hero";
 function App() {
   return (
     <>
+      <div className="absolute inset-0 -z-10 h-full w-full bg-[#fffbf2] bg-[linear-gradient(to_right,#faeded_1px,transparent_1px),linear-gradient(to_bottom,#faeded_1px,transparent_1px)] bg-[size:6rem_4rem]"></div>
       <Header />
       <Hero />
     </>
