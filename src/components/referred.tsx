cd7ca00f376faa6ffefa3c6e98f4eaae51e421a1
Franco Gilli <francogilli10@gmail.com>
2024-07-21 16:41:02 -0300

    Agregar color a la seccion referidos

--- src/components/referred.tsx
+++ src/components/referred.tsx
@@ -1,9 +1,9 @@
 export default function Referred() {
   return (
-    <div className="py-10 md:space-x-6 gap-y-2 flex flex-col md:flex-row justify-center items-center bg-gradient-to-br from-[#fbf5e9] to-[#fffbf2] rounded-3xl border border-[#ffdede]">
-      <p className="text-4xl font-bold tracking-tight">Recomenda y gana</p>
-      <p className="md:w-44 font-medium">Recibi $25.000 para vos y tu amigo.</p>
-      <button className="bg-gradient-to-r from-[#1c1c1c] to-[#3d3d3d] text-white font-medium rounded-2xl text-sm px-7 mt-5 md:mt-0 py-3 text-center">
+    <div className="py-10 md:space-x-6 gap-y-2 flex flex-col md:flex-row justify-center items-center fondo rounded-3xl border border-[#ffdede]">
+      <p className="text-4xl font-bold tracking-tight text-white">¡Recomenda y gana!</p>
+      <p className="md:w-44 font-medium text-white">Recibi $25.000 para vos y tu amigo.</p>
+      <button className="fondo2 border-[2px] border-[#353535] text-white font-normal rounded-2xl text-sm px-7 mt-5 md:mt-0 py-3 text-center">
         Ingresar ahora
       </button>
     </div>
