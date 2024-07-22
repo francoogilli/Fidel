d5ccacf9e3449d1bf5f0d08f89fb1d69396c1de1
Franco Gilli <francogilli10@gmail.com>
2024-07-22 18:08:01 -0300

    Cambiar estilos botones del hero

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -35,13 +35,10 @@ export default function Hero() {
 
       <div>
         <div className="flex flex-col lg:flex-row lg:space-x-3.5 mx-auto justify-center items-center text-center space-y-2 lg:space-y-0">
-          <button className="bg-zinc-800 hover:scale-105 duration-500 hover:bg-[#141414] pl-4 px-2 py-2 text-sm text-white rounded-3xl flex items-center space-x-2">
-            <span>Ingresar ahora</span>
-            <span className="p-2 hover:scale-110 duration-500">
-              <ChevronIcon />
-            </span>
+          <button className="bg-zinc-800 hover:scale-105 duration-500 hover:bg-[#141414] px-6 py-3 text-[13px] text-white rounded-2xl flex items-center space-x-2">
+            Empezar ahora
           </button>
-          <button className="font-medium text-black border hover:scale-105 duration-500 border-zinc-100 bg-gradient-to-r from-[#35fa32]  via-[#35fa32] to-[#ff9500] px-6 py-3 text-sm rounded-3xl flex items-center space-x-2">
+          <button className="font-medium text-black border hover:scale-105 duration-500 border-zinc-100 bg-gradient-to-r from-[#35fa32]  via-[#35fa32] to-[#ff9500] px-6 py-3 text-[13px] rounded-2xl flex items-center space-x-2">
             <span>¡Pedi una demo!</span>
           </button>
         </div>
