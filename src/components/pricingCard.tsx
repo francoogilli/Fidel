ac525cbe107168e48d222b524d53a6a1332efdda
Franco Gilli <francogilli10@gmail.com>
2024-07-25 17:37:06 -0300

    Quitar animacion a las Pricing cards

--- src/components/pricingCard.tsx
+++ src/components/pricingCard.tsx
@@ -102,7 +102,7 @@ export default function PricingCard() {
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-7">
         {pricingData.map((pricing) => (
           <div
-            className="animation-1 flex flex-col p-6 text-center bg-gradient-to-br from-[#f7f7f7b9] to-[#fff] rounded-3xl border border-[#ededed] hover:scale-105 duration-500"
+            className="flex flex-col p-6 text-center bg-gradient-to-br from-[#f7f7f7b9] to-[#fff] rounded-3xl border border-[#ededed] hover:scale-105 duration-500"
             key={pricing.name}
           >
             <h3 className="text-3xl text-left font-bold bg-gradient-to-r from-[#1c1c1c] to-[#3d3d3d] bg-clip-text text-transparent">
