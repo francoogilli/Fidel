641afc8fa3b75a7e228c7ba66d9b3787d3bcb8e3
Franco Gilli <francogilli10@gmail.com>
2024-07-28 15:23:14 -0300

    Ajustar responsividad para dispositivos mobiles

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,17 +3,17 @@ import { socialLinks, appLinks, footerLinks } from "../data/data";
 export default function Footer() {
   return (
     <footer className="bg-[#191919] max-w-8xl mx-4 md:mx-5 mb-4 md:mb-5 rounded-[1.875rem] text-white p-4">
-      <div className="flex flex-col md:flex-row justify-between px-3 md:px-24 pb-6 md:pb-16 pt-20">
+      <div className="flex flex-col md:flex-row justify-between px-3 md:px-24 pb-6 md:pb-16 pt-7 md:pt-20">
         <div className="flex flex-col justify-start max-w-72">
           <div className="flex justify-start items-center space-x-4">
             <img src="/fidel1.svg" width={80} alt="Fidel Logo" />
             <p className="text-3xl font-medium">Fidel</p>
           </div>
-          <p className="text-[#a9a9a9] text-base font-medium pt-5">
+          <p className="text-[#a9a9a9] text-sm md:text-base font-medium pt-5">
             Empowering Your Projects, Enhancing Your Success, Every Step of the
             Way.
           </p>
-          <div className="flex justify-start items-center space-x-3 pt-6">
+          <div className="flex justify-start items-center space-x-3 pt-6 pb-6">
             {socialLinks.map((link, index) => (
               <a
                 key={index}
@@ -26,13 +26,13 @@ export default function Footer() {
             ))}
           </div>
         </div>
-        <div className="flex space-x-24">
+        <div className="grid grid-cols-2 sm:grid-cols-3 gap-x-20 gap-y-7 md:pb-0 pb-7">
           {footerLinks.map((section, index) => (
             <div key={index}>
               <h3 className="text-base text-[#999999] font-normal pb-3.5">
                 {section.title}
               </h3>
-              <ul className="space-y-3">
+              <ul className="space-y-2 md:space-y-3">
                 {section.links.map((link, index) => (
                   <li key={index}>
                     <a
