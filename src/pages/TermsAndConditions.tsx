8ad34dde29387263d2798191ea653de9019e7b07
Franco Gilli <francogilli10@gmail.com>
2024-08-07 22:12:42 -0300

    Agregar contenido

--- src/pages/TermsAndConditions.tsx
+++ src/pages/TermsAndConditions.tsx
@@ -1,5 +1,56 @@
 export const TermsAndConditions = () => {
-  return <>
-    <h3 className="text-6xl font-semibold pt-20 justify-center flex" style={{ fontFamily: 'Outfit Variable' }}>Terms & Conditions</h3>
-  </>;
-};
+    return (
+      <div className="bg-white max-w-5xl mx-auto px-4" style={{ fontFamily: 'Outfit Variable' }}>
+        <h3 className="text-4xl md:text-6xl font-medium pt-24 justify-center flex pb-2" style={{ fontFamily: 'Outfit Variable' }}>
+          Términos & Condiciones
+        </h3>
+        <div className="pt-2 md:pt-8 text-lg">
+          <p className="text-[#242726] pb-12 md:pb-24 font-light text-base flex justify-center">
+            Última Actualización: 04 de febrero, 2024
+          </p>
+  
+          <h4 className="font-semibold text-[1.7rem] my-8">1. Introducción:</h4>
+          <p className="text-[#242726] font-light text-base">
+            El Programa de Red de Vendedores otorga a cualquier persona mayor de 18 años ("Participante") un Bono único e intransferible por el valor de $10.000 (pesos argentinos). El mismo es comunicado y otorgado por la empresa Agencia Programarte SAS, fundadora de Fidel. Esta iniciativa busca incentivar la promoción y expansión de los servicios de Fidel, ofreciendo una recompensa a aquellos que logren atraer nuevos clientes.
+          </p>
+          <p className="text-[#242726] font-light text-base">
+            Para participar, se deberán aceptar los siguientes términos y condiciones. Estos términos están diseñados para asegurar un proceso justo y transparente para todos los participantes, garantizando que las referencias sean genuinas y que los bonos se otorguen de acuerdo con las reglas establecidas.
+          </p>
+  
+          <h4 className="font-semibold text-[1.7rem] my-8">2. Requisitos para obtener el bono:</h4>
+          <p className="text-[#242726] font-light text-base">
+            El Participante puede conseguir un bono solo si:
+          </p>
+          <ul className="list-disc list-inside text-[#242726] font-light text-base">
+            <li>
+              Refiere a un usuario no vinculado anteriormente a través del formulario disponible en los distintos medios (Página Web, App Móvil, Sistema ERP) y este contrata por primera vez un plan de los ofrecidos en el año corriente. El usuario referido debe ser una persona que nunca haya tenido una relación comercial con Fidel anteriormente.
+            </li>
+          </ul>
+  
+          <h5 className="font-semibold text-[1.5rem] my-8">Ampliación del párrafo anterior:</h5>
+          <p className="text-[#242726] font-light text-base">
+            El Participante no podrá reclamar el pago del premio si, anteriormente, el Usuario potencial fue referido por otra persona o si ya tuvo relación comercial con el producto Fidel. En caso de que el usuario referido haya sido contactado previamente o haya utilizado los servicios de Fidel, el bono no será otorgado. De activarse cualquiera de las dos situaciones, será notificado por el ente emisor del programa “Red de Vendedores”.
+          </p>
+  
+          <h4 className="font-semibold text-[1.7rem] my-8">3. Proceso de referencia:</h4>
+          <p className="text-[#242726] font-light text-base">
+            El Participante debe ingresar los datos solicitados del Usuario potencial, a fin de contactarlo y generar el vínculo. En caso de no obtener respuesta por parte de este último, queda cancelado el premio en cuestión. Es fundamental que los datos proporcionados sean correctos y completos para facilitar el proceso de contacto y confirmación.
+          </p>
+          <p className="text-[#242726] font-light text-base">
+            También se solicitará, por única vez, la cuenta bancaria y correo electrónico del “Participante”, para la posterior comunicación y transferencia del premio. Esta información será utilizada exclusivamente para la gestión del bono y no será compartida con terceros.
+          </p>
+  
+          <h4 className="font-semibold text-[1.7rem] my-8">4. Entrega del premio:</h4>
+          <p className="text-[#242726] font-light text-base">
+            La entrega del premio se realizará mediante correo electrónico. Se enviará el comprobante de transferencia bancaria al e-mail de contacto del “Participante” una vez que el Usuario potencial contrate un plan. El proceso de transferencia se completará dentro de los 30 días hábiles posteriores a la confirmación de la nueva contratación.
+          </p>
+  
+          <h4 className="font-semibold text-[1.7rem] my-8">5. Vigencia del acuerdo:</h4>
+          <p className="text-[#242726] font-light text-base">
+            Este acuerdo es válido desde el 1 de enero de 2023 hasta el 31 de diciembre de 2023. Durante este período, los participantes podrán referir nuevos usuarios y optar por los bonos correspondientes siempre que se cumplan las condiciones mencionadas. Al finalizar el año, los términos y condiciones podrán ser revisados y actualizados según las políticas de la empresa.
+          </p>
+        </div>
+      </div>
+    );
+  };
+  
