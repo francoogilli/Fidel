7958f60725e828bf7281360dc591b52e4591970f
Franco Gilli <francogilli10@gmail.com>
2024-08-11 20:23:57 -0300

    Move archivos de lugar

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
-    rollupOptions: {
-      output: {
-        assetFileNames: 'assets/[name].[ext]'
-      }
-    },
+    outDir: 'dist',
+  },
+  server: {
+    open: true,
   }
-})
+});
