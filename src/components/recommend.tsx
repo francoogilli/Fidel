cb21fcdf5262204bc11dfe25f25f5c64efc53995
Franco Gilli <francogilli10@gmail.com>
2024-08-16 21:55:17 -0300

    Quitar hover

--- src/components/recommend.tsx
+++ src/components/recommend.tsx
@@ -77,7 +77,7 @@ export default function Recommend() {
           persona que se suscriba usando tu enlace.
         </p>
         <Link to="/terms">
-          <button className="flex justify-center items-center gap-x-2 mx-auto bg-[#212121] text-white font-normal rounded-2xl duration-300 text-sm px-7 py-3 mt-5 text-center">
+          <button className="flex justify-center items-center gap-x-2 mx-auto bg-[#212121] text-white font-normal rounded-2xl text-sm px-7 py-3 mt-5 text-center">
             Descubri más
             <ArrowUpIcon className="size-4"/>
           </button>
@@ -156,7 +156,7 @@ export default function Recommend() {
           persona que se suscriba usando tu enlace.
         </p>
         <Link to="/terms">
-          <button className="flex justify-center items-center mx-auto text-xs bg-gradient-to-r gap-x-1.5 from-[#000000] to-[#434343] text-white hover:scale-105 duration-300 font-normal rounded-2xl px-7 py-3 mt-5 text-center">
+          <button className="flex justify-center items-center mx-auto text-xs bg-gradient-to-r gap-x-1.5 from-[#000000] to-[#434343] text-white font-normal rounded-2xl px-7 py-3 mt-5 text-center">
             Descubri más
             <ArrowUpIcon className="size-4"/>
           </button>
@@ -237,7 +237,7 @@ export default function Recommend() {
           persona que se suscriba usando tu enlace.
         </p>
         <Link to="/terms">
-          <button className="flex justify-center items-center mx-auto px-7 py-4 mt-5 w-full text-sm bg-gradient-to-r from-[#000000] to-[#323232] text-white hover:scale-105 duration-300 gap-x-1.5 font-normal rounded-[0.875rem] text-center">
+          <button className="flex justify-center items-center mx-auto px-7 py-4 mt-5 w-full text-sm bg-gradient-to-r from-[#000000] to-[#323232] text-white gap-x-1.5 font-normal rounded-[0.875rem] text-center">
             Descubri más
             <ArrowUpIcon className="size-4"/>
           </button>
