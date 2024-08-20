ab6166826074843bf400f21bbe70bbc0a9cd6146
Franco Gilli <francogilli10@gmail.com>
2024-08-20 20:43:16 -0300

    Cambiar texto

--- src/components/callToAction.tsx
+++ src/components/callToAction.tsx
@@ -22,8 +22,8 @@ export default function CallToAction() {
           className="text-center mx-auto text-base md:text-lg pt-1 w-[22rem] md:w-[30rem] font-medium text-[#717171]"
           style={{ fontFamily: "Satoshi" }}
         >
-          Manage your analytics & sales all in one place and transform your
-          business with Scalable.
+          Gestiona tus operaciones y ventas en un solo lugar y transforma tu
+          empresa con Fidel.
         </h6>
         <button
           className="bg-[#5fff5c] mx-auto mt-4 md:mt-5 tracking-tighter md:tracking-normal text-[#171717] justify-center w-auto font-bold py-2.5 px-6 md:px-7 md:py-2.5 text-base md:text-[15px] rounded-[18px] flex items-center space-x-2 border-4 md:border-[5px] border-[#ffffff] hover:border-[#c9ffc8] transition-all duration-700"
