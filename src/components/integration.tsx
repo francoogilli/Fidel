9fc952b8d88f1688f075c3e1cb4adc2b41b974b7
Franco Gilli <francogilli10@gmail.com>
2024-10-03 17:20:14 -0300

    Ajustar imagen en mobile

--- src/components/integration.tsx
+++ src/components/integration.tsx
@@ -6,7 +6,7 @@ import TiendaNubeIcon from "../icons/tiendaNube";
 
 export default function Integration() {
   return (
-    <div className="max-w-7xl mb-8 mx-auto">
+    <div className="max-w-7xl mb-8 mx-auto px-4">
       <div className="flex justify-center pt-20">
         <div className="flex items-center rounded-lg bg-[#F8F9FA] border border-[#E6E9EE] px-3 py-1 animation-1">
           <IntegrationIcon className="size-4 rotate-90 -scale-x-100 text-[#333333]" />
@@ -18,7 +18,7 @@ export default function Integration() {
           </h2>
         </div>
       </div>
-      <div className="flex mx-auto justify-center items-center pb-12">
+      <div className="flex mx-auto justify-center items-center pb-2 md:pb-12">
         <h3
           className="animation-1 text-4xl md:text-[3.25rem] font-bold text-center my-8 pb-1 tracking-tight"
           style={{ fontFamily: "Satoshi" }}
@@ -34,7 +34,7 @@ export default function Integration() {
         </h3>
       </div>
       <div className="grid grid-cols-1 md:grid-cols-12 relative gap-x-5">
-        <div className="col-span-6 relative border border-[#ffffff] rounded-3xl">
+        <div className="col-span-6 relative hidden md:block border border-[#ffffff] rounded-3xl">
           <div className="absolute flex justify-center bg-[#fbfbfb] px-12 py-4 rounded-[2rem] hover:scale-105 top-5 left-16 transition-all duration-300">
             <TiendaNubeIcon className="size-28" fill="#212121" />
           </div>
@@ -60,6 +60,13 @@ export default function Integration() {
               alt=""
             />
           </div>
+        </div>
+        <div className="col-span-6 md:hidden flex justify-center items-center mx-auto">
+          <img
+            src="integrationimage.png"
+            alt="Imagen para móviles"
+            className="w-full h-auto"
+          />
         </div>
         <div className="col-span-6 bg-gradient-to-br from-[#fcfef9] via-[#F8F9FA] to-[#fcfef9] border border-[#e6eee6] rounded-3xl px-6 py-5">
           <h3
