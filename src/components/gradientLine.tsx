a1b5e5e0b79e898d9c2b1bcd49bd9160b05588a1
Franco Gilli <francogilli10@gmail.com>
2024-10-08 17:01:57 -0300

    Cambiar color line gradient

--- src/components/gradientLine.tsx
+++ src/components/gradientLine.tsx
@@ -6,7 +6,7 @@ interface GradientLineProps {
     const gradientDirection = direction === 'left' ? 'bg-gradient-to-l' : 'bg-gradient-to-r';
   
     return (
-      <div className={`${gradientDirection} from-[#eff5fd] via-[#eff5fd]/100 to-[#eff5fd]/0 flex-grow flex-shrink-0 h-[1px] overflow-hidden relative w-[1px] animation-1`}></div>
+      <div className={`${gradientDirection} from-[#e6eaf0] via-[#e6eaf0]/100 to-[#e6eaf0]/0 flex-grow flex-shrink-0 h-[1px] overflow-hidden relative w-[1px] animation-1`}></div>
     );
   }
   
