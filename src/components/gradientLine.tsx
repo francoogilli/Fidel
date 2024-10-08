6992097961e71245fb1a3e8dbb895840b2a41e74
Franco Gilli <francogilli10@gmail.com>
2024-10-07 23:08:42 -0300

    Agregar animation

--- src/components/gradientLine.tsx
+++ src/components/gradientLine.tsx
@@ -6,7 +6,7 @@ interface GradientLineProps {
     const gradientDirection = direction === 'left' ? 'bg-gradient-to-l' : 'bg-gradient-to-r';
   
     return (
-      <div className={`${gradientDirection} from-[#eff5fd] via-[#eff5fd]/100 to-[#eff5fd]/0 flex-grow flex-shrink-0 h-[1px] overflow-hidden relative w-[1px]`}></div>
+      <div className={`${gradientDirection} from-[#eff5fd] via-[#eff5fd]/100 to-[#eff5fd]/0 flex-grow flex-shrink-0 h-[1px] overflow-hidden relative w-[1px] animation-1`}></div>
     );
   }
   
