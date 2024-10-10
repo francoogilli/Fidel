876e1cc5a671aafcc56cf5dcc4f1bfaac903fcb3
Franco Gilli <francogilli10@gmail.com>
2024-10-10 17:55:38 -0300

    Cambiar color tabla

--- src/components/pricingComparision.tsx
+++ src/components/pricingComparision.tsx
@@ -1,57 +1,75 @@
-import Check from '../icons/checkIcon';
-import Cross from '../icons/cross';
-import Tooltip from './tooltip';
-import InfoCircle from '../icons/infoCircle';
-import { pricingComparison } from '../data/data';
-
+import Check from "../icons/checkIcon";
+import Cross from "../icons/cross";
+import Tooltip from "./tooltip";
+import InfoCircle from "../icons/infoCircle";
+import { pricingComparison } from "../data/data";
 
 export default function PricingComparison() {
-    return (
-      <div className="max-w-7xl mx-auto px-4 pb-10 pt-3">
-        <div className="flex items-center justify-between">
-      </div>
-        <div className="overflow-x-auto rounded-3xl border border-[#e8e8e8]">
-          <table className="min-w-full divide-y divide-[#e8e8e8] rounded-3xl overflow-hidden">
-            <thead className="bg-gray-50">
-              <tr>
-                <th className="p-4 px-12 text-left font-medium rounded-tl-3xl">Características</th>
-                {pricingComparison.map((plan, index) => (
-                  <th key={index} className={`p-4 px-10 md:px-20 text-center font-medium ${index === pricingComparison.length - 1 ? 'rounded-tr-3xl' : ''}`}>
-                    {plan.name}
-                  </th>
+  return (
+    <div className="max-w-7xl mx-auto px-4 pb-10 pt-3">
+      <div className="flex items-center justify-between"></div>
+      <div className="overflow-x-auto rounded-3xl border border-[#e8e8e8]">
+        <table className="min-w-full divide-y divide-[#e8e8e8] rounded-3xl overflow-hidden">
+          <thead className="bg-gray-50">
+            <tr>
+              <th className="p-4 px-12 text-left font-medium rounded-tl-3xl">
+                Características
+              </th>
+              {pricingComparison.map((plan, index) => (
+                <th
+                  key={index}
+                  className={`p-4 px-10 md:px-20 text-center font-medium ${
+                    index === pricingComparison.length - 1
+                      ? "rounded-tr-3xl"
+                      : ""
+                  }`}
+                >
+                  {plan.name}
+                </th>
+              ))}
+            </tr>
+          </thead>
+          <tbody className="bg-[#fdfdfd] divide-y divide-[#e8e8e8]">
+            {pricingComparison[0].benefits.map((benefit, index) => (
+              <tr key={index}>
+                <td className="p-4 text-sm justify-start flex items-center px-6 md:px-12 space-x-1.5">
+                  <span>{benefit.text}</span>
+                  <Tooltip
+                    text={benefit.tooltipInfo}
+                    hideOnSmallScreens={false}
+                  >
+                    <InfoCircle />
+                  </Tooltip>
+                </td>
+                {pricingComparison.map((plan, planIndex) => (
+                  <td
+                    key={planIndex}
+                    className={`p-4 text-center ${
+                      index === pricingComparison[0].benefits.length - 1
+                        ? "rounded-bl-3xl"
+                        : ""
+                    }`}
+                  >
+                    {plan.benefits[index]?.displayText ? (
+                      <span className="text-sm">
+                        {plan.benefits[index].displayText}
+                      </span>
+                    ) : plan.benefits[index]?.active ? (
+                      <div className="flex items-center justify-center">
+                        <Check className="bg-gradient-to-b from-[#63ff33] to-[#b3ff99] rounded-full p-1" />
+                      </div>
+                    ) : (
+                      <div className="flex items-center justify-center">
+                        <Cross />
+                      </div>
+                    )}
+                  </td>
                 ))}
               </tr>
-            </thead>
-            <tbody className="bg-[#fdfdfd] divide-y divide-[#e8e8e8]">
-              {pricingComparison[0].benefits.map((benefit, index) => (
-                <tr key={index}>
-                  <td className="p-4 text-sm justify-start flex items-center px-6 md:px-12 space-x-1.5">
-                    <span>{benefit.text}</span>
-                    <Tooltip text={benefit.tooltipInfo} hideOnSmallScreens={false}>
-                      <InfoCircle />
-                    </Tooltip>
-                  </td>
-                  {pricingComparison.map((plan, planIndex) => (
-                    <td key={planIndex} className={`p-4 text-center ${index === pricingComparison[0].benefits.length - 1 ? 'rounded-bl-3xl' : ''}`}>
-                      {plan.benefits[index]?.displayText ? (
-                        <span className='text-sm'>{plan.benefits[index].displayText}</span>
-                      ) : plan.benefits[index]?.active ? (
-                        <div className="flex items-center justify-center">
-                          <Check className="bg-gradient-to-b from-[#b7ff33] to-[#dbff99] rounded-md p-1" />
-                        </div>
-                      ) : (
-                        <div className="flex items-center justify-center">
-                          <Cross />
-                        </div>
-                      )}
-                    </td>
-                  ))}
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        </div>
+            ))}
+          </tbody>
+        </table>
       </div>
-    );
-  };
-  
+    </div>
+  );
+}
