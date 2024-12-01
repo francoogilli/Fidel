c9b7cc50e0ae6df74f04d4ed01af75db106fe698
Franco Gilli <francogilli10@gmail.com>
2024-12-01 12:03:00 -0300

    Prueba

--- src/api/index.ts
+++ src/api/index.ts
@@ -12,7 +12,7 @@ const saveContact = async (
 }> => {
   try {
     const response = await axios.post(
-      "/hubspot/crm/v3/objects/contacts",
+      "https://api.hubapi.com/crm/v3/objects/contacts",
       body,
       {
         headers: {
